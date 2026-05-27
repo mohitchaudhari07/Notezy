14178b449b62dddb9a9a1bea736ead3f57180c38
Mohit Chaudhari <166442441+mohitchaudhari07@users.noreply.github.com>
2026-05-27 15:56:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import { AppProvider } from "./context/AppContext";
+import ReactGA from "./utils/analytics";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+    });
+  }, []);
+  
   return (
     <AuthProvider>
       <AppProvider>
